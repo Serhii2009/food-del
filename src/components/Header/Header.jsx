686ed91f91beddcc9d3f4186ed3b0bf79d01f7fb
Serhii2009 <137280424+Serhii2009@users.noreply.github.com>
2024-05-28 15:39:34 +0300

    add some components and pages

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-contents">
+        <h2>Order your favorite food here</h2>
+        <p>
+          Welcome to our restaurant! We offer a variety of delicious dishes made
+          with fresh ingredients and traditional recipes. Our friendly staff is
+          ready to serve you. Come and enjoy a memorable dining experience with
+          us!
+        </p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
