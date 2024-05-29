cf884f2f5d1c47067907e3f669fd84c0136a5e5a
Serhii2009 <137280424+Serhii2009@users.noreply.github.com>
2024-05-29 12:40:05 +0300

    add item counter

--- src/components/FoodItem/FoodItem.jsx
+++ src/components/FoodItem/FoodItem.jsx
@@ -1,12 +1,39 @@
+import { useState } from 'react'
 import { assets } from '../../assets/assets'
 import './FoodItem.css'
 
 // eslint-disable-next-line no-unused-vars
 const FoodItem = ({ id, name, price, description, image }) => {
+  const [itemCount, setitemCount] = useState(0)
+
   return (
     <div className="food-item">
       <div className="food-item-img-container">
         <img className="food-item-image" src={image} alt="" />
+        {!itemCount ? (
+          <img
+            className="add"
+            onClick={() => setitemCount((prev) => prev + 1)}
+            src={assets.add_icon_white}
+            alt=""
+          />
+        ) : (
+          <div className="food-item-counter">
+            <img
+              onClick={() => setitemCount((prev) => prev - 1)}
+              src={assets.remove_icon_red}
+              alt=""
+            />
+
+            <p>{itemCount}</p>
+
+            <img
+              onClick={() => setitemCount((prev) => prev + 1)}
+              src={assets.add_icon_green}
+              alt=""
+            />
+          </div>
+        )}
       </div>
       <div className="food-item-info">
         <div className="food-item-name-rating">
