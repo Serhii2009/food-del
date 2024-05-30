de6f1f959305e9aeeefc9ae14c11d622940be429
Serhii2009 <137280424+Serhii2009@users.noreply.github.com>
2024-05-30 18:03:15 +0300

    add cart page with styles and logic

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,18 @@
 import './Navbar.css'
 import { assets } from '../../assets/assets'
-import { useState } from 'react'
+import { useContext, useState } from 'react'
 import { Link } from 'react-router-dom'
+import { StoreContext } from '../../context/StoreContext'
 
 const Navbar = ({ setshowLogin }) => {
   const [menu, setMenu] = useState('home')
+  const { getTotalCartAmount } = useContext(StoreContext)
 
   return (
     <div className="navbar">
-      <img src={assets.logo} alt="" className="logo" />
+      <Link to="/">
+        <img src={assets.logo} alt="" className="logo" />
+      </Link>
 
       <ul className="navbar-menu">
         <Link
@@ -47,8 +51,10 @@ const Navbar = ({ setshowLogin }) => {
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
         <div className="navbar-search-icon">
-          <img src={assets.basket_icon} alt="" />
-          <div className="dot"></div>
+          <Link to="/cart">
+            <img src={assets.basket_icon} alt="" />
+          </Link>
+          <div className={getTotalCartAmount() === 0 ? '' : 'dot'}></div>
         </div>
 
         <button onClick={() => setshowLogin(true)}>sign in</button>
