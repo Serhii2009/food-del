da0d3a180e374a51f038380cb262fc087c7fc3f4
Serhii2009 <137280424+Serhii2009@users.noreply.github.com>
2024-07-16 12:06:17 +0300

    add

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,5 +1,16 @@
+import Navbar from './components/Navbar/Navbar'
+import Sidebar from './components/Sidebar/Sidebar'
+
 const App = () => {
-  return <div></div>
+  return (
+    <div>
+      <Navbar />
+      <hr />
+      <div className="app-content">
+        <Sidebar />
+      </div>
+    </div>
+  )
 }
 
 export default App
