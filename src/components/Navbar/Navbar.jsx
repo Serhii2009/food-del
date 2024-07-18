5467c7c974d01214b1e57f11647df69b5f88fe06
Serhii2009 <137280424+Serhii2009@users.noreply.github.com>
2024-07-18 20:30:48 +0300

    add full stack logic of navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,10 +3,18 @@ import { assets } from '../../assets/assets'
 import { useContext, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { StoreContext } from '../../context/StoreContext'
+import { useNavigate } from 'react-router-dom'
 
 const Navbar = ({ setshowLogin }) => {
   const [menu, setMenu] = useState('home')
-  const { getTotalCartAmount } = useContext(StoreContext)
+  const { getTotalCartAmount, token, setToken } = useContext(StoreContext)
+
+  const navigate = useNavigate()
+  const logout = () => {
+    localStorage.removeItem('token')
+    setToken('')
+    navigate('/')
+  }
 
   return (
     <div className="navbar">
@@ -57,7 +65,24 @@ const Navbar = ({ setshowLogin }) => {
           <div className={getTotalCartAmount() === 0 ? '' : 'dot'}></div>
         </div>
 
-        <button onClick={() => setshowLogin(true)}>sign in</button>
+        {!token ? (
+          <button onClick={() => setshowLogin(true)}>sign in</button>
+        ) : (
+          <div className="navbar-profile">
+            <img src={assets.profile_icon} alt="" />
+            <ul className="nav-profile-dropdown">
+              <li>
+                <img src={assets.bag_icon} alt="" />
+                <p>Orders</p>
+              </li>
+              <hr />
+              <li onClick={logout}>
+                <img src={assets.logout_icon} alt="" />
+                <p>Logout</p>
+              </li>
+            </ul>
+          </div>
+        )}
       </div>
     </div>
   )
