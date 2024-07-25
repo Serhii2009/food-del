858b46724ca07657b76314553db6db6a79fe7fc3
Serhii2009 <137280424+Serhii2009@users.noreply.github.com>
2024-07-25 14:30:57 +0300

    add food-orders ui and logic for remove reloading

--- src/pages/PlaceOrder/PlaceOrder.jsx
+++ src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,6 +1,7 @@
-import { useContext, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import './PlaceOrder.css'
 import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 import { StoreContext } from '../../context/StoreContext'
 
 const PlaceOrder = () => {
@@ -51,6 +52,16 @@ const PlaceOrder = () => {
       alert('Error')
     }
   }
+
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (!token) {
+      navigate('/cart')
+    } else if (getTotalCartAmount() === 0) {
+      navigate('/cart')
+    }
+  }, [token])
 
   return (
     <form onSubmit={placeOrder} className="place-order">
