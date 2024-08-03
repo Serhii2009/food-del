18db8abbd794ca133a76569ee996ee30b08be020
Serhii2009 <137280424+Serhii2009@users.noreply.github.com>
2024-08-03 15:00:36 +0300

    Revert "Merge branch 'main' of https://github.com/Serhii2009/food-del"
    This reverts commit 65a011cc130c65137546327d08d2ae537834bd75, reversing
    changes made to 8358c083bfd1aa84ee86e3edaec972d1eb5fe52b.

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null)
 
 export const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({})
-  const url = 'https://food-del-backend-csxi.onrender.com'
+  const url = 'http://localhost:4000'
   const [token, setToken] = useState('')
   const [food_list, setFoodList] = useState([])
 
